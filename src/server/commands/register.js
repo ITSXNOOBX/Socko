be1a453c39b223e8324511336b30d1ea2cc8eeee
ITSXNOOBX <arangojond@gmail.com>
2023-11-29 13:23:03 +0000

    Working on random codes

--- src/server/commands/register.js
+++ src/server/commands/register.js
@@ -1,9 +1,39 @@
 // src/server/actions/register.js
+const crypto = require('crypto');
+
+const g = require('../../globals')
+const mongo = require('../../db/mongo')
 
 const register = (socket) => {
-    socket.on('register', (userData) => {
-        console.log(userData)
+    const log = g.getLog();
+
+    socket.on('register', (ud) => {
+        if (!ud.email || !ud.password)
+            return;
+
+        if (!isHash(ud.email) || !isHash(ud.password))
+            return;
+
+        const new_user = {
+            email: user_data.email,
+            password: user_data.password,
+            code: 'ABC123',
+        }
+
+        try {
+            // mongo.TeacherData.create(new_user);
+        } catch (e) {
+            log.error(e)
+        }
     });
+};
+
+const isHash = (str) => {
+    return typeof str === 'string' && str.length === 64;
+};
+
+const randCode = () => {
+    return crypto.randomBytes(8).toString('hex');
 };
 
 module.exports.register = register;
--- src/server/socket.js
+++ src/server/socket.js
@@ -16,11 +16,12 @@ module.exports.start = async () => {
     log.success("Socket initializer started.")
 
     io.on('connection', (socket) => {
-        const origin = socket.handshake.headers.origin;
-        // if (!allowed_headers.includes(origin)) {
-        //     socket.emit('invalidConnection', { success: false, message: 'Invalid connection' });
-        //     return socket.disconnect();
-        // }
+        const origin = socket.handshake.query.token;
+        if (!allowed_headers.includes(origin)) {
+            socket.emit('error', { success: false, message: 'Invalid connection' });
+            log.console(`Connection rejected due to invalid quiery token: ${socket.handshake.address}`)
+            return socket.disconnect();
+        }
 
         if (process.env.NODE_ENV === 'development')
             log.console(`Socket connected: ${socket.id}, origin: ${origin}`)
@@ -30,7 +31,6 @@ module.exports.start = async () => {
         * For better modularization of the code
         */
         _register.register(socket);
-      
       
         /**
         * @brief Test socket connection
